880c9eeed84cdf3acde4c8689ac3b4977898c422
Ercan <kadri503394@gmail.com>
2023-01-27 17:39:47 +0300

    feat: added delete category operation

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -2,7 +2,7 @@ const Category = require("../models/Category.js");
 const express = require("express");
 const router = express.Router();
 
-
+// !GET
 router.get("/get-all", async (req,res)=>{
   try{
     const categories = await Category.find()
@@ -11,8 +11,7 @@ router.get("/get-all", async (req,res)=>{
     res.status(400).json(error)
   }
 })
-
-
+// !CREATE
 router.post("/add-category", async (req,res) => {
   try{ 
    const newCategory = new Category(req.body);
@@ -22,7 +21,7 @@ router.post("/add-category", async (req,res) => {
     res.status(400).json(error)
   }
 })
-
+// !UPDATE
 router.put("/update-category", async (req,res)=>{
   try{
     await Category.findOneAndUpdate({_id: req.body._id}, req.body)
@@ -31,6 +30,14 @@ router.put("/update-category", async (req,res)=>{
     res.status(400).json(error)
   }
 })
-
+// !DELETE
+router.delete("/delete-category", async (req,res)=>{
+  try{
+    await Category.findOneAndDelete({_id: req.body._id})
+    res.status(200).json("Item deleted successfully")
+  }catch(error){
+    res.status(400).json(error)
+  }
+})
 
 module.exports = router;
