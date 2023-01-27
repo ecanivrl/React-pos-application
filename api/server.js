6c0ea5ed8fcd4c1107a1482a6d8822e1854ac29c
Ercan <kadri503394@gmail.com>
2023-01-27 12:18:41 +0300

    feat: added integration mongodb

--- api/server.js
+++ api/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
+mongoose.set("strictQuery", true)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+//! Connect to MongoDB
+const connect = async () => {
+    try{
+         await mongoose.connect("mongodb+srv://EcaniVRL:Kv5033.,@cluster0.v1z0hbp.mongodb.net/?retryWrites=true&w=majority")
+         console.log('MongoDB connected');
+    }catch(error){
+        console.error('MongoDB connection error', error);
+    }
+}
+ 
+
+app.get('/', (req, res) => {res.send('Hello World!');});
 
 app.listen(port, () => {
+    connect();
     console.log(`Example app listening at on port ${port}`);
     });
