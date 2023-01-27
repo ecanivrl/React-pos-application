13bba21e440e67f603b54f43db36565de8c2042e
Ercan <kadri503394@gmail.com>
2023-01-27 16:05:53 +0300

    feat: added get all categories operations

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -3,7 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 
-
+router.get("/get-all", async (req,res)=>{
+  try{
+    const categories = await Category.find()
+    res.status(200).json(categories)
+  }catch(error){
+    res.status(400).json(error)
+  }
+})
 
 
 router.post("/add-category", async (req,res) => {
