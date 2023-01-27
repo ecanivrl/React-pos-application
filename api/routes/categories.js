282a24afaecf2130339f066761f7e76f7c6096c0
Ercan <kadri503394@gmail.com>
2023-01-27 17:31:29 +0300

    feat: added update categories operation

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -23,4 +23,14 @@ router.post("/add-category", async (req,res) => {
   }
 })
 
+router.put("/update-category", async (req,res)=>{
+  try{
+    await Category.findOneAndUpdate({_id: req.body._id}, req.body)
+    res.status(200).json("Item updated successfully")
+  }catch(error){
+    res.status(400).json(error)
+  }
+})
+
+
 module.exports = router;
