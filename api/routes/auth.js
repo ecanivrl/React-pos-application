d04bbc167720082b337caec86e43157815db03be
Ercan <kadri503394@gmail.com>
2023-01-31 12:06:49 +0300

    feat: added hashed password operation

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,21 @@
 const User = require("../models/User.js");
 const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
 
 
 // !Register Create User 
 router.post("/register", async (req,res) => {
-  try{ 
-   const newUser = new User(req.body);
-   await newUser.save();
-   res.status(200).json("A new user created successfully")
+  try{
+    const {username, email, password} = req.body;
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    });
+    await newUser.save();
+    res.status(200).json("A new user created successfully")
   }catch(error){
     res.status(400).json(error)
   }
