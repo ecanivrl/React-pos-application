09dd1e256d1fcc1e8ac73633b0d828aee778f2c4
Ercan <kadri503394@gmail.com>
2023-02-14 03:48:31 +0300

    feat: added api link of bil details

--- api/models/Bill.js
+++ api/models/Bill.js
@@ -5,6 +5,7 @@ const BillSchema = mongoose.Schema(
     customerName: { type: String, require: true },
     customerPhoneNumber: { type: String, require: true },
     paymentMode: { type: String, require: true },
+    productTitle: { type: String, require: true },
     cartItems: { type: Array, require: true },
     subTotal: { type: Number, require: true },
     tax: { type: Number, require: true },
--- client/src/pages/BillPage.jsx
+++ client/src/pages/BillPage.jsx
@@ -1,41 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/header/Header';
-import { Button, Card,  Table } from 'antd';
+import { Button, Table } from 'antd';
 import PrintBill from '../components/bills/PrintBill';
 
 const BillPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [billItems, setBillItems] = useState()
 
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ];
+  useEffect(() => {
+    const getBillItems = async () => {
+      try {
+        const res = await fetch(`http://localhost:5000/api/bills/get-all`);
+        const data = await res.json();
+        setBillItems(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBillItems();
+  }, []);
+
+
+
   const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: 'Müşteri Adı',
+      dataIndex: 'customerName',
+      key: 'customerName',
+      render: (text) => <span className='text-xs'>{text}</span>,
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
+      title: 'Telefon Numarası',
+      dataIndex: 'customerPhoneNumber',
+      key: 'customerPhoneNumber',
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
+      title: 'Oluşturma Tarihi',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      render: (text) => <span>{new Date(text).toLocaleDateString()}</span>,
     },
+    {
+      "title": "Sipariş Edilen Ürün",
+      "dataIndex": "productTitle",
+      "key": "productTitle",
+    },
+   
+    {
+      title: "Ödeme Şekli",
+      dataIndex: "paymentMode",
+      key: "paymentMode",
+    },
+    {
+      title: 'Toplam Fiyat',
+      dataIndex: "totalAmount",
+      key: "totalAmount",
+      render: (text) => <span className='font-bold'>{text} ₺</span>,
+    },
+    {
+      title: 'İşlemler',
+      dataIndex: 'actions',
+      key: 'actions',
+      render: (text, record) => (
+        <div className='flex justify-center'>
+          <Button
+            onClick={() => setIsModalOpen(true)}
+            type="primary"  
+            className="mr-2"
+            size="small"
+          >
+            Yazdır
+          </Button>
+        </div>
+      ),
+    }
+   
   ];
 
   return (
@@ -44,24 +83,12 @@ const BillPage = () => {
       <div className="px-6">
         <h1 className='text-4xl font-bold text-center mb-4'>Faturalar</h1>
         <Table
-          dataSource={dataSource}
+          dataSource={billItems}
           columns={columns}
           bordered
           pagination={false}
         />
-        <div className="cart-total flex justify-end mt-4">
-          <Card className="w-72">
-           
-            <Button
-              onClick={() => setIsModalOpen(true)}
-              type="primary"
-              className="mt-4 w-full"
-              size="large"
-            >
-              Sipariş Oluştur
-            </Button>
-          </Card>
-        </div>
+        
       </div>
      <PrintBill setIsModalOpen={setIsModalOpen} isModalOpen={isModalOpen}/>
     </>
