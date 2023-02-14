68d039f03f37a48491e44c80bc95d5981f19a370
Ercan <kadri503394@gmail.com>
2023-02-15 01:22:23 +0300

    feat: added logout with route control

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { SearchOutlined, HomeOutlined,ShoppingCartOutlined, CopyOutlined, UserOutlined, BarChartOutlined, LogoutOutlined } from '@ant-design/icons';
-import { Badge, Input } from 'antd';
-import {Link} from "react-router-dom"
+import {
+  SearchOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  CopyOutlined,
+  UserOutlined,
+  BarChartOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons';
+import { Badge, Input, Popconfirm, message } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
-  const cart = useSelector((state) => state.cart)
+  const cart = useSelector((state) => state.cart);
+  const navigate = useNavigate();
+  const LogOut = () => {
+    if (localStorage.getItem('posUser')) {
+      localStorage.removeItem('posUser');
+      message.success('Çıkış Yapıldı');
+      setTimeout(() => {
+        navigate('/login');
+      }, 1200);
+    }
+  };
+
   return (
     <div className="border-b mb-6">
       <header className="py-4 px-6 flex justify-between items-center gap-10">
@@ -19,44 +38,79 @@ const Header = () => {
             size="large"
             placeholder="Ürün ara..."
             prefix={<SearchOutlined />}
-            className='rounded-full max-w-[800px]'
+            className="rounded-full max-w-[800px]"
           />
         </div>
-        <div className="menu-links flex justify-between items-center md:gap-7 gap-3 md:static z-50 fixed bottom-0 md:w-auto w-screen
-        md:bg-transparent bg-white left-0 md:px-0 px-8 border-t md:border-none py-1 md:py-0">
-          <Link to={"/"} className='menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center'>
-            <HomeOutlined className='md:text-2xl text-xl' />
-            <span className='md:text-xs text-[10px]'>Ana Sayfa</span>
+        <div
+          className="menu-links flex justify-between items-center md:gap-7 gap-3 md:static z-50 fixed bottom-0 md:w-auto w-screen
+        md:bg-transparent bg-white left-0 md:px-0 px-8 border-t md:border-none py-1 md:py-0"
+        >
+          <Link
+            to={'/'}
+            className="menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center"
+          >
+            <HomeOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">Ana Sayfa</span>
           </Link>
-          <Badge count={cart.cartItems.length} offset={[0,0]} className='md:flex hidden'>
-          <Link to={"/cart"} className='menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center'>
-            <ShoppingCartOutlined className='md:text-2xl text-xl' />
-            <span className='md:text-xs text-[10px]'>Sepet</span>
-          </Link>
+          <Badge
+            count={cart.cartItems.length}
+            offset={[0, 0]}
+            className="md:flex hidden"
+          >
+            <Link
+              to={'/cart'}
+              className="menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center"
+            >
+              <ShoppingCartOutlined className="md:text-2xl text-xl" />
+              <span className="md:text-xs text-[10px]">Sepet</span>
+            </Link>
           </Badge>
-          <Link to={"/bills"} className='menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center'>
-            <CopyOutlined  className='md:text-2xl text-xl' />
-            <span className='md:text-xs text-[10px]'>Faturalar</span>
+          <Link
+            to={'/bills'}
+            className="menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center"
+          >
+            <CopyOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">Faturalar</span>
           </Link>
-          <Link to={"/customers"} className='menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center'>
-            <UserOutlined  className='md:text-2xl text-xl' />
-            <span className='md:text-xs text-[10px]'>Müşteriler</span>
+          <Link
+            to={'/customers'}
+            className="menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center"
+          >
+            <UserOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">Müşteriler</span>
           </Link>
-          <Link to={"/statistic"} className='menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center'>
-            <BarChartOutlined  className='md:text-2xl text-xl' />
-            <span className='md:text-xs text-[10px]'>İstatistikler</span>
+          <Link
+            to={'/statistic'}
+            className="menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center"
+          >
+            <BarChartOutlined className="md:text-2xl text-xl" />
+            <span className="md:text-xs text-[10px]">İstatistikler</span>
           </Link>
-          <Link to={"/login"} className='menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center'>
-            <LogoutOutlined className='md:text-2xl text-xl' />
-            <span className='md:text-xs text-[10px]'>Çıkış</span>
-          </Link>
+          <Popconfirm
+            title="Çıkış Yapmak İstediğinize Emin Misiniz?"
+            onConfirm={LogOut}
+            okText="Evet"
+            cancelText="Hayır"
+          >
+            <div className="menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center">
+              <LogoutOutlined className="md:text-2xl text-xl" />
+              <span className="md:text-xs text-[10px]">Çıkış</span>
+            </div>
+          </Popconfirm>
         </div>
-        <Badge count={cart.cartItems.length} offset={[0,0]} className='md:hidden flex'>
-          <Link to={"/cart"} className='menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center'>
-            <ShoppingCartOutlined className='text-2xl ' />
-            <span className='md:text-xs text-[10px]'>Sepet</span>
+        <Badge
+          count={cart.cartItems.length}
+          offset={[0, 0]}
+          className="md:hidden flex"
+        >
+          <Link
+            to={'/cart'}
+            className="menu-link hover:text-[#40a9ff] flex flex-col transition-all duration-500 justify-center items-center"
+          >
+            <ShoppingCartOutlined className="text-2xl " />
+            <span className="md:text-xs text-[10px]">Sepet</span>
           </Link>
-          </Badge>
+        </Badge>
       </header>
     </div>
   );
