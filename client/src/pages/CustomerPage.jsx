6d14b2fb1ded00143403f49a573c3629e2a96302
Ercan <kadri503394@gmail.com>
2023-02-15 05:30:15 +0300

    feat: added filtered in customer page

--- client/src/pages/CustomerPage.jsx
+++ client/src/pages/CustomerPage.jsx
@@ -1,12 +1,131 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Header from '../components/header/Header';
-import { Table, message } from 'antd';
+import { Button, Input, Space, Table, message } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import Highlighter from 'react-highlight-words';
+import {  SearchOutlined } from '@ant-design/icons';
 
 const CustomerPage = () => {
   const naigate = useNavigate();
   const [billItems, setBillItems] = useState([]);
   const [status, setStatus] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+  
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
 
   useEffect(() => {
     const getBillItems = async () => {
@@ -33,6 +152,7 @@ const CustomerPage = () => {
       title: 'Müşteri Adı',
       dataIndex: 'customerName',
       key: 'customerName',
+      ...getColumnSearchProps('customerName'),
       render: (text) => (
         <div
         key={text._id}
@@ -62,12 +182,14 @@ const CustomerPage = () => {
       title: 'Telefon Numarası',
       dataIndex: 'customerPhoneNumber',
       key: 'customerPhoneNumber',
+      ...getColumnSearchProps('customerPhoneNumber'),
     },
     {
-      title: 'İşelem Tarihi',
+      title: 'İşlem Tarihi',
       dataIndex: 'createdAt',
       key: 'createdAt',
-      render: (text) => <span>{new Date(text).toLocaleDateString()}</span>,
+      sorter: (a, b) => new Date(a.createdAt) - new Date(b.createdAt),
+      render: (text) => <span>{new Date(text).toLocaleDateString(0 , 10)}</span>,
     },
     {
       title: 'İşlem Saati',
@@ -79,6 +201,8 @@ const CustomerPage = () => {
       title: 'İşlem Tutarı',
       dataIndex: 'totalAmount',
       key: 'totalAmount',
+      sorter: (a, b) => a.totalAmount - b.totalAmount,
+      ...getColumnSearchProps('totalAmount'),
       render: (text) => <span>{text.toFixed(2)}₺</span>,
     },
   ];
